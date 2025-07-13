6b03d2d9b79a6a673b7e40c0d77c42e7146d6204
abdulrahim-0 <abdarhim2003@gmail.com>
2025-07-14 00:28:32 +0300

    chore: remove unused import statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Counter from './components/counter';
 function App() {
-  return (
-    <Counter initialValue={0} step={1} />
-  );
+  return <Counter initialValue={0} step={1} />;
 }
 
 export default App;
