125b2bae11279410a9cd659ca57583eaf57dcc47
abdarhim <abdarhim2003@gmail.com>
2025-07-21 22:02:50 +0300

    chore: install Material UI and refactor Counter component to use it

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,92 +1,52 @@
 import { JSX, useState } from 'react';
 import NumberFact from './NumberFact';
+import { Button, Grid, ButtonGroup, Typography } from '@mui/material';
 
 interface CounterProps {
   initialValue: number;
   step: number;
 }
 
-function Counter({initialValue , step}: CounterProps): JSX.Element {
+function Counter({ initialValue, step }: CounterProps): JSX.Element {
   const [counter, setCounter] = useState<number>(initialValue);
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: '#181a1b',
-        fontFamily: 'Arial, sans-serif',
-        color: '#f5f6fa'
-      }}
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center" // horizontally
+      alignItems="center" // vertically
+      direction="column"
+      style={{ minHeight: '100vh', background: '#181A1B' }} // fill full screen height
     >
-      <h1 style={{ fontSize: '2.5rem', color: '#f5f6fa', marginBottom: '20px' }}>
-        Counter App
-      </h1>
-      <div
+      <Grid size={12} style={{ textAlign: 'center' }}>
+        <Typography variant="h4" style={{ textAlign: 'center' , color: '#FFFFFF' }}>
+          Counter App
+        </Typography>
+      </Grid>
+      <Grid size={12} style={{ textAlign: 'center' }}>
+        <Typography variant="h2" style={{ color: '#FFFFFF' }}>{counter}</Typography>
+      </Grid>
+      <Grid size={12} style={{ textAlign: 'center' }}>
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Button onClick={() => setCounter(counter + step)}>Increment</Button>
+          <Button color='error' onClick={() => setCounter(counter - step)}>Decrement</Button>
+        </ButtonGroup>
+      </Grid>
+      <Grid
+        container={true}
+        size={12}
         style={{
-          fontSize: '2rem',
-          color: '#e1e1e6',
-          marginBottom: '30px',
-          padding: '10px 20px',
-          backgroundColor: '#23272f',
-          borderRadius: '8px',
-          boxShadow: '0 4px 12px rgba(0,0,0,0.5)'
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
-        {counter}
-      </div>
-      <div>
-        <button
-          style={{
-            padding: '12px 25px',
-            fontSize: '1rem',
-            cursor: 'pointer',
-            margin: '10px',
-            border: 'none',
-            borderRadius: '5px',
-            backgroundColor: '#2563eb',
-            color: '#f5f6fa',
-            boxShadow: '0 4px 8px rgba(0,0,0,0.3)',
-            transition: 'background-color 0.3s ease'
-          }}
-          onClick={() => setCounter(counter + step)}
-          onMouseOver={(e) =>
-            (e.currentTarget.style.backgroundColor = '#1e40af')
-          }
-          onMouseOut={(e) =>
-            (e.currentTarget.style.backgroundColor = '#2563eb')
-          }
-        >
-          Increment
-        </button>
-        <button
-          style={{
-            padding: '12px 25px',
-            fontSize: '1rem',
-            cursor: 'pointer',
-            margin: '10px',
-            border: 'none',
-            borderRadius: '5px',
-            backgroundColor: '#dc2626',
-            color: '#f5f6fa',
-            boxShadow: '0 4px 8px rgba(0,0,0,0.3)',
-            transition: 'background-color 0.3s ease'
-          }}
-          onClick={() => setCounter(counter - step)}
-          onMouseOver={(e) =>
-            (e.currentTarget.style.backgroundColor = '#991b1b')
-          }
-          onMouseOut={(e) =>
-            (e.currentTarget.style.backgroundColor = '#dc2626')
-          }
-        >
-          Decrement
-        </button>
-      </div>
-      <NumberFact value={counter} />
-    </div>
+        <NumberFact value={counter} />
+      </Grid>
+    </Grid>
   );
 }
 
